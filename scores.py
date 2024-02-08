30d90b662329f27ec1a87fca4d26e9a3c8cae78a
Hailin Liu <70336326+lhl4971@users.noreply.github.com>
2024-02-08 22:35:06 +0300

    Delete scores.py

--- scores.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-def print_scores(title, pred, test):
-    data = np.array([accuracy_score(pred, test), precision_score(pred, test), recall_score(pred, test), f1_score(pred, test)])
-    df = pd.DataFrame(data)
-    df.to_csv("./data.csv", index = False, mode = 'a')
